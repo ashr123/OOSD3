ef56bf6785957fc2c71ee78cea2c455ef0065fae
Roy <roykid123@gmail.com>
2017-05-25 11:59:02 +0300

    sasd

--- src/Board.java
+++ src/Board.java
@@ -11,44 +11,45 @@ public class Board extends JPanel
 {
 	private final LevelLoader loader=new LevelLoader();
 	static Cell[][] board;
-	private JLabel[][] level;
+	private JLabel[][] jLabels;
+	private static int level;
 	private int playerLocationX, playerLocationY;
 	
-	Board(int level) throws IOException
+	Board(int jLabels) throws IOException
 	{
 		loader.load("levels.txt");
-		board=loader.get(level);
+		board=loader.get(jLabels);
 		buildBoard();
 	}
 	
 	void buildBoard()
 	{
 		setLayout(new GridLayout(board[0].length, board.length));
-		level=new JLabel[board.length][board[0].length];
+		jLabels=new JLabel[board.length][board[0].length];
 		for (int i=0; i<board.length; i++)
 			for (int j=0; j<board[i].length; j++)
 			{
 				if (!board[i][j].isFloor())//is a wall
 				{
-					level[i][j]=new JLabel(new ImageIcon("WallBlack.png"));
-					//add(level[i][j]);
+					jLabels[i][j]=new JLabel(new ImageIcon("WallBlack.png"));
+					//add(jLabels[i][j]);
 					continue;
 				}
 				if (board[i][j].isStorage() && board[i][j].hasBox())//Storage with box
 				{
-					level[i][j]=new JLabel(new ImageIcon("StorageWithBox.png"));
+					jLabels[i][j]=new JLabel(new ImageIcon("StorageWithBox.png"));
 					continue;
 				}
 				if (board[i][j].isStorage() && !board[i][j].hasBox() && !board[i][j].hasPlayer())//Storage without a box
 				{
-					level[i][j]=new JLabel(new ImageIcon("Storage.png"));
-					//add(level[i][j]);
+					jLabels[i][j]=new JLabel(new ImageIcon("Storage.png"));
+					//add(jLabels[i][j]);
 					continue;
 				}
 				if (board[i][j].isEmptyFloor())//Is empty floor
 				{
-					level[i][j]=new JLabel(new ImageIcon("Floor.png"));
-					//add(level[i][j]);
+					jLabels[i][j]=new JLabel(new ImageIcon("Floor.png"));
+					//add(jLabels[i][j]);
 					continue;
 				}
 				if (board[i][j].hasPlayer() && !board[i][j].hasBox())//Player on floor
@@ -59,8 +60,8 @@ public class Board extends JPanel
 				}
 				if (board[i][j].hasBox())//Box on the floor
 				{
-					level[i][j]=new JLabel(new ImageIcon("Box.png"));
-					//add(level[i][j]);
+					jLabels[i][j]=new JLabel(new ImageIcon("Box.png"));
+					//add(jLabels[i][j]);
 					continue;
 				}
 				if (!board[i][j].hasPlayer() && board[i][j].isStorage())//Player on storage
@@ -70,8 +71,8 @@ public class Board extends JPanel
 				}
 			}
 		
-		for (int i=0; i<level[0].length; i++)
-			for (JLabel[] aLevel : level)
+		for (int i=0; i<jLabels[0].length; i++)
+			for (JLabel[] aLevel : jLabels)
 				add(aLevel[i]!=null ? aLevel[i] : new JLabel());
 		addKeyListener(new KeyListener()
 		{
@@ -98,6 +99,14 @@ public class Board extends JPanel
 					case KeyEvent.VK_DOWN:
 						
 						break;
+				}
+				try
+				{
+					Game.refreshBoard(level);
+				}
+				catch (IOException e1)
+				{
+					e1.printStackTrace();
 				}
 			}
 			
