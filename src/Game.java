92d2606dee460cfab416c3d7f044a16d7df9246e
Roy <roykid123@gmail.com>
2017-05-26 13:45:25 +0300

    Reformating

--- src/Game.java
+++ src/Game.java
@@ -15,10 +15,10 @@ public class Game extends JFrame
 		super("Sokoban");
 		Board.playerState=PlayerState.FRONT;
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		counter = new JLabel(numberOfSteps+"");
+		counter=new JLabel(numberOfSteps+"");
 		Board board=new Board(0);
 		JSplitPane paneOfPanes=new JSplitPane(JSplitPane.VERTICAL_SPLIT, counter, new LevelsPane(this));
-		pane=new JSplitPane(JSplitPane.HORIZONTAL_SPLIT , paneOfPanes, board);
+		pane=new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, paneOfPanes, board);
 		add(pane);
 		setResizable(false);
 		pack();
@@ -28,7 +28,8 @@ public class Game extends JFrame
 		pane.getRightComponent().requestFocus();
 	}
 	
-	static int getNumberOfSteps(){
+	static int getNumberOfSteps()
+	{
 		return numberOfSteps;
 	}
 	
@@ -50,10 +51,14 @@ public class Game extends JFrame
 			exc.printStackTrace(System.out);
 		}
 	}
-	static JLabel getCounter(){
+	
+	static JLabel getCounter()
+	{
 		return counter;
 	}
-	static void setNumberOfSteps(){
-		numberOfSteps =0;
+	
+	static void setNumberOfSteps()
+	{
+		numberOfSteps=0;
 	}
 }
