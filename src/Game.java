396fb46729ba4cf90f8fb45568476b40cf227427
Saarule <saaryazdi@gmail.com>
2017-05-26 15:33:33 +0300

    Uml

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,16 @@
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.*;
+import javax.swing.undo.UndoManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
 public class Game extends JFrame
 {
+	JButton undo;
+	UndoManager undoManager;
 	static int numberOfSteps;
 	private static JLabel counter;
 	static JSplitPane pane;
@@ -16,8 +21,25 @@ public class Game extends JFrame
 		Board.playerState=PlayerState.FRONT;
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		counter=new JLabel(numberOfSteps+"");
+		
+		undoManager = new UndoManager();
+		undo = new JButton("Undo");
+		JPanel comp = new JPanel();
+		comp.add(counter);
+		comp.add(undo);
+		undo.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				if (undoManager.canUndo()){
+					numberOfSteps--;
+					undoManager.undo();
+				}
+			}
+		});
 		Board board=new Board(0);
-		JSplitPane paneOfPanes=new JSplitPane(JSplitPane.VERTICAL_SPLIT, counter, new LevelsPane(this));
+		JSplitPane paneOfPanes=new JSplitPane(JSplitPane.VERTICAL_SPLIT, comp, new LevelsPane(this));
 		pane=new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, paneOfPanes, board);
 		add(pane);
 		setResizable(false);
@@ -27,6 +49,7 @@ public class Game extends JFrame
 		pane.getRightComponent().setFocusable(true);
 		pane.getRightComponent().requestFocus();
 	}
+	
 	
 	static int getNumberOfSteps()
 	{
